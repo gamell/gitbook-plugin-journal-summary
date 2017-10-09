69a3c6658656be0c169bc37c692a80f384e03e73
Joan Gamell <gamell@users.noreply.github.com>
2017-10-08 19:34:43 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # gitbook-plugin-journal-summary
 Plugin to generate gitbook's summary file for journal-style entries
+
+## Setup
+
+## Config
+
+## Usage
+
+## Credit
+
+Heavily influenced by [julianxhokaxhiu's gitbook plugin](https://github.com/julianxhokaxhiu/gitbook-plugin-summary).
---
+++ index.js
@@ -0,0 +1,72 @@
+// const fs = require('fs'),
+//       glob = require('glob'),
+//       path = require('path'),
+//       Parser = require('markdown-parser')
+//
+// function generateEntry( title, path, readmeFilename ) {
+//   let depth = path.match(/\//g).length
+//
+//   if ( path.indexOf( readmeFilename ) == -1 )
+//     depth++
+//
+//   return `${Array(depth).join('    ')}- [${title}](${path})\n`
+// }
+//
+// module.exports = {
+//   hooks: {
+//     init: function () {
+//       const parser = new Parser(),
+//             root = this.resolve(''),
+//             bookTitle = this.config.get('title'),
+//             readmeFilename = this.config.get('structure.readme'),
+//             summaryFilename = this.config.get('structure.summary')
+//
+//       let ret = Promise.resolve(),
+//           summaryContent = ( bookTitle ? `# ${bookTitle}\n\n` : '' )
+//
+//       glob(
+//         `*/**/*.md`,
+//         {
+//           cwd: root,
+//           ignore: ['node_modules/**']
+//         },
+//         ( err, files ) => {
+//           files.forEach( ( filePath ) => {
+//             ret = ret.then(
+//               () => {
+//                 return new Promise(
+//                   ( resolve, reject ) => {
+//                     parser.parse(
+//                       fs.readFileSync( `${root}/${filePath}`, { encoding: 'utf8' } ),
+//                       ( err, result ) => {
+//                         if ( result.headings.length ) {
+//                           const fileTitle = result.headings[0].trim()
+//
+//                           summaryContent += generateEntry( fileTitle, filePath, readmeFilename )
+//                         }
+//
+//                         resolve()
+//                       }
+//                     )
+//                   }
+//                 )
+//               }
+//             )
+//           })
+//
+//           ret = ret.then(
+//             () => {
+//               fs.writeFileSync( `${root}/${summaryFilename}`, summaryContent, { encoding: 'utf8' } )
+//
+//               console.log(`\x1b[36mgitbook-plugin-summary: \x1b[32m${summaryFilename} generated successfully.`)
+//
+//               return Promise.resolve()
+//             }
+//           )
+//         }
+//       )
+//
+//       return ret;
+//     }
+//   }
+// }
