f795d4e0f0f9280b573734e099de90fe9f4ba1a7
Joan Gamell <gamell@@users.noreply.github.com>
2017-10-16 08:38:56 -0700

    Moving to bsummaryTree

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const glob = require('glob');
 const path = require('path');
+const moment = require('moment');
 const Parser = require('markdown-parser');
 const parser = new Parser();
 
@@ -34,46 +35,67 @@ function parse({ contents, filePath }) {
 function getTitle({ parsed, filePath }) {
   let title = '';
   if ( parsed.headings.length ) {
-    title = parsed.headings[0].trim()
+    title = parsed.headings[0].trim();
   }
   return Promise.resolve({ title, filePath });
 };
 
-function getSummaryEntry({ title, filePath }){
-  let depth = filePath.match(/\//g).length;
-  if (filePath.indexOf(readmeFilename) === -1) {
-    depth++;
-  }
-  const summaryEntry = `${Array(depth).join('    ')}- [${title}](${filePath})\n`
-  return Promise.resolve(summaryEntry);
+function getEntry({ title, filePath }){
+  let fileName = path.parse(filePath).name;
+  let date = moment(fileName);
+  console.log(`DATE: ${date}`)
+  const entry = {
+    year: date.year(),
+    month: date.month(),
+    day: date.date(),
+    title,
+    filePath
+  };
+  return Promise.resolve(entry);
 };
 
 function processFile(filePath) {
   return readFile(`${rootPath}/${filePath}`)
     .then(parse)
     .then(getTitle)
-    .then(getSummaryEntry)
+    .then(getEntry)
     .catch((reason) => {
-      console.error(reason);
       throw reason;
     });
 };
 
-function asyncAppend(prev, curr){
-  return prev.then((s0) => curr.then((s1) => s0 + s1));
-}
+function buildTree(prev, curr) {
+  return
+    prev.then((tree) =>
+      curr.then((node) => {
+        const { year, month, day, title, filePath } = node;
+        tree[year] = { title: year, filePath: '' } || tree[year];
+        tree[year][month] = { title: month, filePath: '' } || tree[year][month];
+        tree[year][month][day] = { title, filePath };
+        return tree;
+      });
+    );
+};
 
-function generateSummary() {
+function generateSummaryTree() {
   return new Promise((resolve, reject) => {
     glob(`*/**/*.md`, { cwd: rootPath, ignore: ['node_modules/**'] }, (err, files) => {
       if (err) return reject(err);
-      return files
-        .map(processFile)
-        .reduce(asyncAppend, Promise.resolve('')) // initial value
-        .then(resolve);
+      return files.map(processFile).reduce(buildTree, {}).then(resolve);
     });
   });
 };
+
+function printTree(summaryTree, res = '', depth = 1) {
+  const node = Object.entries(summaryTree);
+  const root = node.shift(); // get first element of the array
+  res += `${Array(depth).join('  ')}- [${root.title}](${root.filePath || ''})\n`
+  depth++;
+  node.forEach((children) => { // for each children of the node, we repeat
+    res += printTree(children, res, depth);
+  });
+  return res;
+}
 
 async function init() {
   const bookTitle = this.config.get('title');
@@ -81,8 +103,9 @@ async function init() {
   rootPath = this.resolve('');
   readmeFilename = this.config.get('structure.readme');
 
-  summary = ( bookTitle ? `# ${bookTitle}\n\n` : '' );
-  summary += await generateSummary();
+  summaryTree = await generateSummaryTree();
+  console.log(`SUMMARY TREE: \n\n${JSON.stringify(summaryTree, null, 2)}`);
+  const summary = ( bookTitle ? `# ${bookTitle}\n\n` : '' ) + printTree(summaryTree);
   fs.writeFileSync( `${rootPath}/${summaryFilename}`, summary, 'utf8');
   console.log(`\x1b[36mgitbook-plugin-journal-summary: \x1b[32m${summaryFilename} generated successfully.`);
   return 0;
--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const chaiAsPromised = require("chai-as-promised");
 const sinon = require('sinon');
 const journalSummary = require('../index.js');
+const fs = require('fs');
 const { execSync } = require('child_process');
 
 const pluginRoot = global.pluginRoot;
@@ -23,17 +24,39 @@ describe('Individual entry journal', function() {
     }
   };
 
-  before(function(){
-    console.log(' *** Cleaning up SUMMARY.md ***');
-    execSync(`rm -rf SUMMARY.md`, { cwd: bookSrc });
+  describe('Return value', function() {
+    it('Should eventually return 0', function() {
+      const res = journalSummary.hooks.init.apply(fakeContext);
+      assert.eventually.equal(res, 0, "Returns zero");
+    });
   });
 
-  it('should run wihtout errors', function() {
-    const res = journalSummary.hooks.init.apply(fakeContext);
-    assert.eventually.equal(res, 0, "Returns zero");
-  });
 
   describe('Summary File', function() {
+    //const summaryFile = fs.readFileSync(`${bookSrc}/SUMMARY.md`, 'utf-8');
+
+    before(function() {
+      //execSync(`rm -rf SUMMARY.md`, { cwd: bookSrc });
+      return journalSummary.hooks.init.apply(fakeContext);
+    });
+
+    after(function() {
+      console.log('*** Cleaning up ***');
+      //execSync(`rm -rf SUMMARY.md`, { cwd: bookSrc });
+    });
+
+    it('Should contain 5 lines', function() {
+      const lines = summaryFile.split('\n').length
+      assert.equal(lines, 8, 'Number of lines is equal');
+    });
+
+    it('Should contain 2016 as level', function() {
+      assert.include(summaryFile, `- [2016](${bookSrc}/2016/)`, 'Contains year as level');
+    });
+
+    it('Should contain 2017 as level', function() {
+      assert.include(summaryFile, `- [2017](${bookSrc}/2017/)`, 'Contains year as level');
+    });
 
   });
 });
